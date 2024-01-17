42919fd94f1594f24e1a698856395a19e1679826
Michael <u21546551@tuks.co.za>
2024-01-17 12:14:39 +0200

    Refactor responsive layout and checkbox styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import { useState, useEffect } from 'react';
 export default function Home() {
 
   const getCurrentTheme = () => (typeof window !== 'undefined' ? window.matchMedia('(prefers-color-scheme: dark)').matches : false);
-  const [isScreenSizeValid, setIsScreenSizeValid] = useState(true);
+  const [isLargeScreen, setIsLargeScreen] = useState(true);
   const [theme, setTheme] = useState<string | null>(null);
 
   useEffect(() => {
-    const handleResize = () => { setIsScreenSizeValid(window.innerWidth >= 1000); };
+    const handleResize = () => { setIsLargeScreen(window.innerWidth >= 1000); };
     handleResize();
 
     window.addEventListener('resize', handleResize);
@@ -20,32 +20,31 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-    {isScreenSizeValid ? 
       <main className={"flex min-h-screen flex-col items-center justify-between overflow-x-hidden " + (theme ? theme : "")}>
-          <Navbar theme={!theme ? getCurrentTheme() : theme === "dark-theme" ? true : false} setTheme={(arg: string) => setTheme(arg)}/>
-          <div className="w-[80%] mt-[100px]">
-            <IntroTextBanner />
-          </div>
-          <Slider />
-          <h1 className="text-center w-[80%] text-[2rem] mt-[100px] mb-[100px] font-black" id="download-section">
-            Download, choose your variant from below
-          </h1>
-          <div className="w-[calc(100vw-20%)] ml-auto mr-auto flex flex-wrap justify-center">
-            { MuzikCards.map((card, index) => <Card key={index} {...card} />) }
-          </div>
-          <h1 className="text-center w-[80%] text-[2rem] mt-[100px] mb-[100px] font-black" id="faq-section">
-            FAQ
-          </h1>
-          { faqCards.map((faq, index) => <FAQ key={index} {...faq} />) }
-          <div className="mb-[100px]" />
-          <Footer />
-        </main>
-        :
-        <main className={"flex min-h-screen items-center justify-center overflow-x-hidden " + (theme ? theme : "")}>
-          <p className="text-[20px] ">Sorry, this website is only available for screens with width greater than 1000px.</p>
-        </main>
-      }
-    </>
+        <Navbar 
+          isLargeScreen={isLargeScreen}
+          theme={!theme ? getCurrentTheme() : theme === "dark-theme" ? true : false} 
+          setTheme={(arg: string) => setTheme(arg)}/>
+        <div className={`w-[${isLargeScreen ? `80%` : `90vw`}] mt-[100px]`}>
+          <IntroTextBanner />
+        </div>
+        { isLargeScreen ?
+            <Slider />
+            :
+            <></>
+        }
+        <h1 className={`text-center w-[${isLargeScreen ? `80%` : `90vw`}] text-[2rem] mt-[100px] mb-[100px] font-black`} id="download-section">
+          Download, choose your variant from below
+        </h1>
+        <div className="w-[calc(100vw-20%)] ml-auto mr-auto flex flex-wrap justify-center">
+          { MuzikCards.map((card, index) => <Card key={index} {...card} />) }
+        </div>
+        <h1 className={`text-center w-[${isLargeScreen ? `80%` : `90vw`}] text-[2rem] mt-[100px] mb-[100px] font-black`} id="faq-section">
+          FAQ
+        </h1>
+        { faqCards.map((faq, index) => <FAQ key={index} {...faq} />) }
+        <div className="mb-[100px]" />
+        <Footer />
+      </main>
   )
 }
